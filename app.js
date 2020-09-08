e6469f9aeab0258ea9ecc450f6865f72dda0e02c
peregurin <chinmayvirmani@gmail.com>
2020-09-08 14:26:53 +0530

    task 1 and task 2 completed
    task 1: hello world program
    task 2: importing modules

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log('Hello World');
+const PrintSomething = require('./utils');
+console.log(PrintSomething);
+console.log(PrintSomething(1,1));
---
+++ utils.js
@@ -0,0 +1,2 @@
+const PrintSomething = (num1, num2) => num1 + num2;
+module.exports = PrintSomething;
